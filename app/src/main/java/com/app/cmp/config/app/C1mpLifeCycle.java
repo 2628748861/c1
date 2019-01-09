7128426a9511019727f01141d8f18fbfe4524798
fu <fuyongjun@gdgyun.com>
2019-01-09 17:05:24 +0800

    提交代码

--- app/src/main/java/com/app/cmp/config/app/C1mpLifeCycle.java
+++ app/src/main/java/com/app/cmp/config/app/C1mpLifeCycle.java
@@ -10,11 +10,12 @@ import com.app.cmp.config.request.HttpApiProxy;
 import com.app.cmp.config.request.RetrofitApi;
 import com.app.library.app.AppLifecycle;
 
-public class CmpLifeCycle implements AppLifecycle {
+public class C1mpLifeCycle implements AppLifecycle {
     @Override
     public void onAttach(Context applicationContext) {
-        Log.e("TAG","初始化:"+CmpLifeCycle.class.getSimpleName());
+        Log.e("TAG","初始化:"+C1mpLifeCycle.class.getSimpleName());
         HttpApiProxy.init(new RetrofitApi());
         DbApiProxy.init(new GreenDaoApi(applicationContext,BuildConfig.DB_NAME));
+
     }
 }
